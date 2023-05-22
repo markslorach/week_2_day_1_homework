83e138a9b5fc4345e602d69014f45b230319ba06
Mark Slorach <hello@markslorach.com>
2023-05-22 16:42:17 +0100

    5 team tests passed

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -3,6 +3,8 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
-        
 
+    def add_player(self, new_player):
+        self.players.append(new_player)
+        
     
